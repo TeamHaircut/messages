6652888b19ee2518fa7c2f549869864f3d2c60d7
teamhaircut <teamhaircutdiginet@gmail.com>
2022-01-09 16:03:21 -0700

    hour2

--- public/DOM/dom.js
+++ public/DOM/dom.js
@@ -1,1 +1,25 @@
+const chatMessages = document.querySelector('.chat-messages');
 
+// Output message to DOM
+function outputMessage(message) {
+	const div = document.createElement('div');
+	if(message.username == getClientUsername()) {
+		div.classList.add('mine');
+	} else {
+		div.classList.add('yours');
+		div.style.fontSize = "small";
+		div.innerHTML =`${message.username}`;
+	}
+
+	div.classList.add('messages');
+	
+	const div1 = document.createElement('div');
+	div1.classList.add("message");
+	div1.classList.add('last');
+	
+	div1.style.fontSize = "medium";
+	//message.time not used
+	div1.innerHTML = `<p>${message.text}</p>`;
+	div.appendChild(div1);
+	document.querySelector('.chat-messages').appendChild(div);
+}
---
+++ public/DOM/utils.js
@@ -0,0 +1,27 @@
+// Get room from template
+const clientRoom = document.getElementById('client-room');
+const room = clientRoom.innerHTML;
+
+// Set ClientRoom element to room
+function setClientRoom(room) {
+	clientRoom.innerText = room;
+}
+
+// Get ClientRoom
+function getClientRoom() {
+    return room;
+}
+
+// Get username from template
+const clientUsername = document.getElementById('client-username');
+var username = clientUsername.innerHTML;
+
+// Set ClientUsername to username
+function setClientUsername(username) {
+    clientUsername.innerHTML = username;
+}
+
+// Get ClientUsername
+function getClientUsername() {
+    return username;
+}
